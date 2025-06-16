b814db032e33f697f0a17fb3525e237986772883
Zypheone94 <maximerene.durand.pro@gmail.com>
2025-06-16 16:36:55 +0200

    [FEAT] : init auth app

---
+++ devops/variables.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCES_KEY_ID" {
+  description = "AWS Acces key"
+  type = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "AWS Secret key"
+  type = string
+  sensitive = true
+}
