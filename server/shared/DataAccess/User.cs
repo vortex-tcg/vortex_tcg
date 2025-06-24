d7a57215054170d20e5fa7bd158521410d6e3013
ValBenj 21 <vfargue668@gmail.com>
2025-06-24 11:29:10 +0200

    [FIX] Model User premier fix

--- server/shared/DataAccess/User.cs
+++ server/shared/DataAccess/User.cs
@@ -1,12 +1,14 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic; //Permet l’utilisation des listes (ICollection<T>) pour les relations 1-N ou N-N.
+using System.ComponentModel.DataAnnotations; //Permet d’utiliser les attributs [Key], [Required], [EmailAddress], etc.
+using System.ComponentModel.DataAnnotations.Schema; //Utilisé pour des attributs comme [ForeignKey] ou [Table] si nécessaire.
+
+
 
 namespace VortexTCG.Models
 {
-    public class User
+    public class User //Cette classe deviendra une table Users (par convention, EF Core met le nom de la classe au pluriel pour la table).
     {
-        [Key]
+        [Key] //Primary Key
         public int Id { get; set; }
 
         [Required]
@@ -15,23 +17,23 @@ namespace VortexTCG.Models
         [Required]
         public string Username { get; set; }
 
-        [Required]
+        [Required] //Met en Not Null, uniquement pour autre chose qu'un INT car ils sont automatiquement non nullable
         public string Password { get; set; }
 
         [Required]
-        [EmailAddress]
+        [EmailAddress] //Ajoute une validation au runtime, mais n'a pas d'effet en base.
         public string Email { get; set; }
-
-        public int Rank { get; set; }
 
         public int CurrencyQuantity { get; set; }
 
         public string Language { get; set; }
 
-        // Relations
+        // Foreign Keys
 
         public int RoleId { get; set; }
         public Role Role { get; set; }
+        public int RankId { get; set; }
+        public Rank Rank { get; set; }
 
         public ICollection<Booster> Boosters { get; set; }
 
