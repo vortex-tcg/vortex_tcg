ed102e3daa537edbebd75c81d9d0f04903eb7980
Zypheone94 <maximerene.durand.pro@gmail.com>
2025-06-27 15:33:30 +0200

    [FEAT] : Ajout de la connexion à la DB pour les services back
    changement pour que les docker files soient homogènes + mise en place des récupération et parsing des variables env pour les différents services

--- server/apps/game/Program.cs
+++ server/apps/game/Program.cs
@@ -1,16 +1,23 @@
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql;
+using System.Text.RegularExpressions;
 using VortexTCG.DataAccess;
+
+Env.Load("../../.env");
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Configuration.AddEnvironmentVariables();
+var replacedConfig = ReplacePlaceholders(builder.Configuration);
+
 builder.Services.AddDbContext<VortexDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+        ServerVersion.AutoDetect(replacedConfig.GetConnectionString("DefaultConnection"))
     ));
 
 var app = builder.Build();
@@ -33,3 +40,26 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+// Funtion to replace ${VAR} with true value
+static IConfiguration ReplacePlaceholders(IConfiguration config)
+{
+    var dict = new Dictionary<string, string?>();
+
+    foreach (var kvp in config.AsEnumerable())
+    {
+        if (kvp.Value is null) continue;
+
+        var newValue = Regex.Replace(kvp.Value, @"\$\{(.+?)\}", match =>
+        {
+            var envVar = match.Groups[1].Value;
+            return Environment.GetEnvironmentVariable(envVar) ?? match.Value;
+        });
+
+        dict[kvp.Key] = newValue;
+    }
+
+    return new ConfigurationBuilder()
+        .AddInMemoryCollection(dict)
+        .Build();
+}
