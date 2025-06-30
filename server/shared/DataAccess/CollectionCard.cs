81138dad947831324cbde673a540a52326c5a291
ValBenj 21 <vfargue668@gmail.com>
2025-06-30 11:14:14 +0200

    [FEAT] Initialisation complète de la DB

--- server/shared/DataAccess/CollectionCard.cs
+++ server/shared/DataAccess/CollectionCard.cs
@@ -4,18 +4,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace VortexTCG.DataAccess.Models
 {
-    public class Booster
+    public class CollectionCard
     {
         [Key]
         public int Id { get; set; }
 
         public string Label { get; set; }
 
+        public int Quantity { get; set; }
+
         public int Price { get; set; }
 
-        public int UserId { get; set; }
-        public User User { get; set; }
-        public int? CardId { get; set; }
-        public Card? Cards { get; set; }
+        public int CardId { get; set; }
+        public Card Card { get; set; }
+
+        public ICollection<Collection> Collections { get; set; }
+
     }
 }
