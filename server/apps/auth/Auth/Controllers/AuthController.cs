5b27f6fa266bd6211fe0a7c9bb486f8071f520cc
ValBenj 21 <vfargue668@gmail.com>
2025-10-22 18:44:48 +0200

    [FIX] AuthController

--- server/apps/auth/Auth/Controllers/AuthController.cs
+++ server/apps/auth/Auth/Controllers/AuthController.cs
@@ -2,11 +2,20 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using VortexTCG.DataAccess;
 
 namespace VortexTCG.Auth.Controllers;
 
 [ApiController]
-public class AuthController(VortexDbContext db, IConfiguration configuration) : ControllerBase
+public class AuthController : ControllerBase
 {
+	private readonly VortexDbContext _db;
+	private readonly IConfiguration _configuration;
 
+	public AuthController(VortexDbContext db, IConfiguration configuration)
+	{
+		_db = db;
+		_configuration = configuration;
+	}
 }
