ed4dc183ebadc32c5ba054a2c7da71a0c5191a1f
Maxime Lombard <maximerene.durand.pro@gmail.com>
2025-10-27 20:13:34 +0100

    [FEAT] : Add create card crud request

--- server/apps/api/Program.cs
+++ server/apps/api/Program.cs
@@ -43,6 +43,8 @@ builder.Services.AddScoped<api.Effect.Providers.EffectTypeProvider>();
 builder.Services.AddScoped<api.Effect.Services.EffectTypeService>();
 builder.Services.AddScoped<api.Effect.Providers.EffectDescriptionProvider>();
 builder.Services.AddScoped<api.Effect.Services.EffectDescriptionService>();
+builder.Services.AddScoped<api.Card.Providers.CardProvider>();
+builder.Services.AddScoped<api.Card.Services.CardService>();
 
 // CORS
 builder.Services.AddCors(options =>
