1627f1659853d8bf6f63029329a480c026768e90
ValBenj 21 <vfargue668@gmail.com>
2025-12-12 15:28:56 +0100

    [FIX] CRUD Cartes

--- server/apps/api/Card/Providers/CardProvider.cs
+++ server/apps/api/Card/Providers/CardProvider.cs
@@ -2,28 +2,33 @@ using Microsoft.EntityFrameworkCore;
 using VortexTCG.DataAccess;
 using CardModel = VortexTCG.DataAccess.Models.Card;
 
-namespace api.Card.Providers
+namespace VortexTCG.Api.Card.Providers
 {
     public class CardProvider
     {
         private readonly VortexDbContext _db;
         public CardProvider(VortexDbContext db) => _db = db;
 
-        public Task<List<CardModel>> listAsync(CancellationToken ct = default)
+        public Task<List<CardModel>> GetAllAsync(CancellationToken ct = default)
         {
             return _db.Cards.AsNoTracking().OrderBy(e => e.Name).ToListAsync(ct);
         }
 
-        public async Task<CardModel> addAsync(CardModel entity, CancellationToken ct = default)
+        public async Task<CardModel> AddAsync(CardModel entity, CancellationToken ct = default)
         {
             _db.Cards.Add(entity);
             await _db.SaveChangesAsync(ct);
             return entity;
         }
 
-        public async Task<bool> existsByNameAsync(string name, CancellationToken ct = default)
+        public async Task<bool> ExistsByNameAsync(string name, CancellationToken ct = default)
         {
             return await _db.Cards.AnyAsync(e => e.Name == name, ct);
+        }
+
+        public Task<CardModel?> GetByIdAsync(Guid id, CancellationToken ct = default)
+        {
+            return _db.Cards.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id, ct);
         }
     }
 }
