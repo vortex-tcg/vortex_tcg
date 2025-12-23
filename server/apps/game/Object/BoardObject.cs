82a2617defbb241f7806181d65416d6b6f62e836
AlexMivelaz <62651957+alexMivelaz@users.noreply.github.com>
2025-12-23 22:33:02 +0100

    [FEATURE]: add attack and defense action to the back

--- server/apps/game/Object/BoardObject.cs
+++ server/apps/game/Object/BoardObject.cs
@@ -10,6 +10,15 @@
 // =============================================
 namespace VortexTCG.Game.Object
 {
+
+    public enum CardSlotState {
+        ENGAGE = 0,
+        ATTACK_ENGAGE = 1,
+        CAN_ATTACK = 2,
+        DEFENSE_ENGAGE = 3,
+        CAN_DEFEND = 4,
+    };
+
     /// <summary>
     /// Plateau de jeu d'un joueur avec 5 emplacements pour les cartes.
     /// </summary>
@@ -20,6 +29,10 @@ namespace VortexTCG.Game.Object
         private Card? _location_3 = null;
         private Card? _location_4 = null;
         private Card? _location_5 = null;
+        private Card? _location_6 = null;
+
+        private List<Card> GetListBoardCards()
+        => new List<Card>([_location_1, _location_2, _location_3, _location_4, _location_5, _location_6]);
 
         /// <summary>
         /// Vérifie si au moins une carte sur le plateau peut attaquer.
@@ -27,12 +40,10 @@ namespace VortexTCG.Game.Object
         /// <returns>True si au moins une carte peut attaquer</returns>
         public bool HasAttackableCards()
         {
-            // Pour l'instant, une carte peut attaquer si elle existe sur le plateau
-            return (_location_1 != null && !_location_1.HasState(CardState.ENGAGE)) ||
-                   (_location_2 != null && !_location_2.HasState(CardState.ENGAGE)) ||
-                   (_location_3 != null && !_location_3.HasState(CardState.ENGAGE)) ||
-                   (_location_4 != null && !_location_4.HasState(CardState.ENGAGE)) ||
-                   (_location_5 != null && !_location_5.HasState(CardState.ENGAGE));
+            List<Card> cards = GetListBoardCards();
+            int count = cards.Count(card => card != null && !card.HasState(CardState.ENGAGE));
+
+            return count > 0;
         }
 
         /// <summary>
@@ -41,12 +52,10 @@ namespace VortexTCG.Game.Object
         /// <returns>True si au moins une carte peut défendre</returns>
         public bool HasDefendableCards()
         {
-            // Pour l'instant, une carte peut défendre si elle existe sur le plateau
-            return (_location_1 != null && !_location_1.HasState(CardState.ATTACK_ENGAGE)) ||
-                   (_location_2 != null && !_location_2.HasState(CardState.ATTACK_ENGAGE)) ||
-                   (_location_3 != null && !_location_3.HasState(CardState.ATTACK_ENGAGE)) ||
-                   (_location_4 != null && !_location_4.HasState(CardState.ATTACK_ENGAGE)) ||
-                   (_location_5 != null && !_location_5.HasState(CardState.ATTACK_ENGAGE));
+            List<Card> cards = GetListBoardCards();
+            int count = cards.Count(card => card != null && !card.HasStates([CardState.ATTACK_ENGAGE, CardState.ENGAGE]));
+
+            return count > 0;
         }
 
         /// <summary>
@@ -54,12 +63,8 @@ namespace VortexTCG.Game.Object
         /// </summary>
         public int GetCardCount()
         {
-            int count = 0;
-            if (_location_1 != null) count++;
-            if (_location_2 != null) count++;
-            if (_location_3 != null) count++;
-            if (_location_4 != null) count++;
-            if (_location_5 != null) count++;
+            List<Card> cards = GetListBoardCards();
+            int count = cards.Count(card => card != null);
             return count;
         }
 
@@ -74,15 +79,17 @@ namespace VortexTCG.Game.Object
         public bool IsAvailable(int location) {
             switch(location) {
                 case 0:
-                    return (_location_1 == null) ? true : false;
+                    return (_location_1 == null);
                 case 1:
-                    return (_location_2 == null) ? true : false;
+                    return (_location_2 == null);
                 case 2:
-                    return (_location_3 == null) ? true : false;
+                    return (_location_3 == null);
                 case 3:
-                    return (_location_4 == null) ? true : false;
+                    return (_location_4 == null);
                 case 4:
-                    return (_location_5 == null) ? true : false;
+                    return (_location_5 == null);
+                case 5:
+                    return (_location_6 == null);
                 default:
                     return false;
             }
@@ -105,31 +112,131 @@ namespace VortexTCG.Game.Object
                 case 4:
                     _location_5 = card;
                     break;
+                case 5:
+                    _location_6 = card;
+                    break;
             }
         }
 
-        private void ResetCardEngageState(Card card) {
-            card.RemoveState(CardState.ENGAGE);
-            card.RemoveState(CardState.ATTACK_ENGAGE);
-            card.RemoveState(CardState.DEFENSE_ENGAGE);
+        public bool TryGetCardPos(int cardId, out int pos) {
+            if (_location_1.GetGameCardId() == cardId) {
+                pos = 0;
+                return true;
+            } else if (_location_2.GetGameCardId() == cardId) {
+                pos = 1;
+                return true;
+            } else if (_location_3.GetGameCardId() == cardId) {
+                pos = 2;
+                return true;
+            } else if (_location_4.GetGameCardId() == cardId) {
+                pos = 3;
+                return true;
+            } else if (_location_5.GetGameCardId() == cardId) {
+                pos = 4;
+                return true;
+            } else if (_location_6.GetGameCardId() == cardId) {
+                pos = 5;
+                return true;
+            } else {
+                pos = -1;
+                return false;
+            }
+        }
+
+        public Card GetCardFromSlot(int slotPos) {
+            switch(slotPos) {
+                case 0:
+                    return _location_1;
+                case 1:
+                    return _location_2;
+                case 2:
+                    return _location_3;
+                case 3:
+                    return _location_4;
+                case 4:
+                    return _location_5;
+                case 5:
+                    return _location_6;
+                default:
+                    return null;
+            }
+        }
+
+        public CardSlotState canAttackSpot(int spot) {
+            switch(spot) {
+                case 0:
+                    return _location_1.checkCanAttack();
+                case 1:
+                    return _location_2.checkCanAttack();
+                case 2:
+                    return _location_3.checkCanAttack();
+                case 3:
+                    return _location_4.checkCanAttack();
+                case 4:
+                    return _location_5.checkCanAttack();
+                case 5:
+                    return _location_6.checkCanAttack();
+                default:
+                    return CardSlotState.CAN_DEFEND;
+            }
+        }
+
+        public CardSlotState canDefendSpot(int spot) {
+            switch(spot) {
+                case 0:
+                    return _location_1.checkCanDefend();
+                case 1:
+                    return _location_2.checkCanDefend();
+                case 2:
+                    return _location_3.checkCanDefend();
+                case 3:
+                    return _location_4.checkCanDefend();
+                case 4:
+                    return _location_5.checkCanDefend();
+                case 5:
+                    return _location_6.checkCanDefend();
+                default:
+                    return CardSlotState.CAN_ATTACK;
+            }
         }
 
         public void ResetBoardEngageState() {
-            if (_location_1 != null) {
-                ResetCardEngageState(_location_1);
-            }
-            if (_location_2 != null) {
-                ResetCardEngageState(_location_2);
-            }
-            if (_location_3 != null) {
-                ResetCardEngageState(_location_3);
-            }
-            if (_location_4 != null) {
-                ResetCardEngageState(_location_4);
-            }
-            if (_location_5 != null) {
-                ResetCardEngageState(_location_5);
+            List<Card> cards = GetListBoardCards();
+
+            foreach (Card card in cards) {
+                if (card != null){
+
+                    card.RemoveStates([CardState.ENGAGE, CardState.ATTACK_ENGAGE, CardState.DEFENSE_ENGAGE]);
+                }
             }
         }
+
+        #region Engage Unengage
+
+            public void EngageAttackCard(int pos) {
+                Card card = GetCardFromSlot(pos);
+
+                card.AddState(CardState.ATTACK_ENGAGE);
+            }
+
+            public void EngageDefenseCard(int pos) {
+                Card card = GetCardFromSlot(pos);
+
+                card.AddState(CardState.DEFENSE_ENGAGE);
+            }
+
+            public void UnEngageAttackCard(int pos) {
+                Card card = GetCardFromSlot(pos);
+
+                card.RemoveState(CardState.ATTACK_ENGAGE);
+            }
+
+            public void UnEngageDefenseCard(int pos) {  
+                Card card = GetCardFromSlot(pos);
+
+                card.RemoveState(CardState.DEFENSE_ENGAGE);
+            }
+
+        #endregion
     }
 }
