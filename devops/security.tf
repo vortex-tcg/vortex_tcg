61f5cf98ca18fb84fdec887fbbfd2a83c360d385
Maxime Lombard <maximerene.durand.pro@gmail.com>
2026-01-23 14:57:22 +0100

    [FEAT] : Add back-end dev configuration

--- devops/security.tf
+++ devops/security.tf
@@ -2,6 +2,10 @@ resource "aws_iam_group" "game_artists" {
   name = "GameArtists"
 }
 
+resource "aws_iam_group" "back_end_dev" {
+  name = "BackEndDevs"
+}
+
 resource "aws_iam_group_policy" "game_artist_policy" {
   name = "game_artist_full_access"
   group = aws_iam_group.game_artists.name
@@ -47,15 +51,74 @@ resource "aws_iam_group_policy" "game_artist_policy" {
   })
 }
 
+resource "aws_iam_group_policy" "back_end_policy" {
+  name = "back-end-server-database-access"
+  group = aws_iam_group.back_end_dev.name
+  
+  policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Sid      = "EC2ReadOnly"
+          Effect   = "Allow"
+          Action   = [
+            "ec2:Describe*",    
+            "ec2:Get*",          
+            "ec2:List*"          
+          ]
+          Resource = "*"
+        },
+        {
+          Sid      = "RDSReadOnly"
+          Effect   = "Allow"
+          Action   = [
+            "rds:Describe*",    
+            "rds:ListTagsForResource",
+            "rds:DownloadDBLogFilePortion" 
+          ]
+          Resource = "*"
+        },
+        {
+          Sid      = "GlobalConsoleAccess"
+          Effect   = "Allow"
+          Action   = [
+            "tag:GetResources",
+            "cloudwatch:GetMetricData",
+            "cloudwatch:ListMetrics",
+            "cloudwatch:GetMetricStatistics" 
+          ]
+          Resource = "*"
+        },
+        {
+          "Effect": "Allow",
+          "Action": [
+            "iam:ChangePassword",
+            "iam:GetAccountPasswordPolicy"
+          ],
+          "Resource": "arn:aws:iam::*:user/$${aws:username}"
+        }
+      ]
+    })
+}
+
 // Add admin group and policy + backend dev
 
 resource "aws_iam_user" "G_Clara" {
   name = "GARCIA.Clara"
 }
 
+resource "aws_iam_user" "J_Mikael" {
+  name = "JARREAU.Mikael"
+}
+
 resource "aws_iam_user_login_profile" "G_Clara_login" {
   user                    = aws_iam_user.G_Clara.name
   password_reset_required = true 
+}
+
+resource "aws_iam_user_login_profile" "J_Mikael_login" {
+  user =aws_iam_user.J_Mikael.name
+  password_reset_required = true
 }
 
 // add user account
@@ -65,10 +128,19 @@ resource "aws_iam_user_group_membership" "G_Clara_membership" {
   groups = [aws_iam_group.game_artists.name]
 }
 
+resource "aws_iam_user_group_membership" "J_Mikael_membership" {
+  user = aws_iam_user.J_Mikael.name
+  groups = [aws_iam_group.back_end_dev.name]
+}
+
 // Access Key 
 
 resource "aws_iam_access_key" "G_Clara_key" {
   user = aws_iam_user.G_Clara.name
+}
+
+resource "aws_iam_access_key" "J_Mikael_key" {
+  user = aws_iam_user.J_Mikael.name
 }
 
 // use to get aws url 
@@ -85,6 +157,21 @@ output "G_Clara_credentials" {
 
     access_key_id = aws_iam_access_key.G_Clara_key.id
     secret_access_key = aws_iam_access_key.G_Clara_key.secret
+  }
+  sensitive = true
+}
+
+output "J_Mikael_credentials" {
+  value = {
+    username = aws_iam_user.J_Mikael.name
+    group = "BackEndDevs"
+
+    console_url       = "https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console"
+    initial_password  = aws_iam_user_login_profile.J_Mikael_login.password
+    password_note     = "Warning you will have to change your password"
+
+    access_key_id = aws_iam_access_key.J_Mikael_key.id
+    secret_access_key = aws_iam_access_key.J_Mikael_key.secret
   }
   sensitive = true
 }
