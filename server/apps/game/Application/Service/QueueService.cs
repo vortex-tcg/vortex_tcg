e1560c26c41bea924ae4ef1e0d44085c4ae7deb4
Ultiorra <jarreaumik@hotmail.com>
2026-02-15 21:56:07 +0100

    feat init match

--- server/apps/game/Application/Service/QueueService.cs
+++ server/apps/game/Application/Service/QueueService.cs
@@ -1,6 +1,9 @@
-﻿using game.Application.Dto;
+﻿using System;
+using System.Collections.Generic;
+using game.Application.Dto;
 using game.Application.Enum;
 using game.Domaine.Interface;
+using game.Domaine.Match.Agregate;
 using game.Domaine.Match.Entity;
 using game.Domaine.Match.ValueObject;
 using game.Domaine.Matchmaking;
@@ -19,42 +22,129 @@ public class QueueService
 
         foreach (IEvent ev in events)
         {
-            if (ev.Name != MatchmakerEvent.FOUND) continue;
+            if (ev.Name != MatchmakerEvent.FOUND)
+            {
+                continue;
+            }
 
             MatchFoundData data = ev.GetData<MatchFoundData>();
+
             Match match = await rm.CreateMatchAsync(data.players, ct);
+
+            match.InitMatch();
+
+            IReadOnlyList<IEvent> matchEvents = match.PullEvents();
+
+            IEvent? initEvent = null;
+            foreach (IEvent me in matchEvents)
+            {
+                if (me.Name == MatchEvent.MATCH_INIT)
+                {
+                    initEvent = me;
+                    break;
+                }
+            }
+
+            if (initEvent == null)
+            {
+                continue;
+            }
+
+            MatchInitData init = initEvent.GetData<MatchInitData>();
+
             UserId p1 = data.players[0].userId;
             UserId p2 = data.players[1].userId;
-            ChampionId p1ChampionId =  new ChampionId(match.Player1.Champion.Id.Value);
-            ChampionId p2ChampionId = new ChampionId(match.Player2.Champion.Id.Value);
-            //TODO : init match, avec toute la data 
-            await CallManager.Instance.CallAsync(new responseDTO<MatchFoundUserDto, MatchFoundUserDto>
+
+            int p1Gold = match.Player1.Champion.Gold.Value;
+            int p2Gold = match.Player2.Champion.Gold.Value;
+
+            int p1Secondary = match.Player1.Champion.SecondaryCurrency.Value;
+            int p2Secondary = match.Player2.Champion.SecondaryCurrency.Value;
+
+            string secondaryName = match.Player1.Champion.SecondaryCurrencyName.Value;
+
+            List<MatchInitCardDto> p1Cards = new List<MatchInitCardDto>(init.Player1DrawnCards.Count);
+            foreach (GameCardDto c in init.Player1DrawnCards)
             {
-                userId = (Guid)p1,
-                opponentId = (Guid)p2,
-                success = true,
-                code = ResponseCode.MATCH_FOUND,
-                data = new MatchFoundUserDto
-                {
-                    matchId = match.MatchId.Value,
-                    championId = p1ChampionId.Value,         
-                    opponentChampionId = p2ChampionId.Value,
-                    opponentHandSize = 5
-                },
-                opponentData = new MatchFoundUserDto
-                {
-                    matchId = match.MatchId.Value,
-                    championId = p2ChampionId.Value,
-                    opponentChampionId = p1ChampionId.Value,
-                    opponentHandSize = 6
-                }
-            }, ct);
+                p1Cards.Add(MatchInitDtoMapper.ToCardDto(c));
+            }
 
+            List<MatchInitCardDto> p2Cards = new List<MatchInitCardDto>(init.Player2DrawnCards.Count);
+            foreach (GameCardDto c in init.Player2DrawnCards)
+            {
+                p2Cards.Add(MatchInitDtoMapper.ToCardDto(c));
+            }
 
+            MatchInitChampionDto p1Champ = MatchInitDtoMapper.ToChampionDto(match.Player1.Champion);
+            MatchInitChampionDto p2Champ = MatchInitDtoMapper.ToChampionDto(match.Player2.Champion);
+
+            responseDTO<MatchInitUserDto, MatchInitUserDto> payload =
+                new responseDTO<MatchInitUserDto, MatchInitUserDto>
+                {
+                    userId = (Guid)p1,
+                    opponentId = (Guid)p2,
+                    success = true,
+                    code = ResponseCode.MATCH_FOUND,
+
+                    data = new MatchInitUserDto
+                    {
+                        matchId = init.MatchId,
+                        opponentHandSize = init.Player2DrawnCards.Count,
+
+                        self = new MatchInitSideDto
+                        {
+                            position = init.Player1Position,
+                            champion = p1Champ,
+                            gold = p1Gold,
+                            secondaryCurrencyName = secondaryName,
+                            secondaryCurrency = p1Secondary,
+                            drawnCards = p1Cards
+                        },
+
+                        opponent = new MatchInitSideDto
+                        {
+                            position = init.Player2Position,
+                            champion = p2Champ,
+                            gold = p2Gold,
+                            secondaryCurrencyName = secondaryName,
+                            secondaryCurrency = p2Secondary,
+                            drawnCards = Array.Empty<MatchInitCardDto>() // pas la main adverse
+                        }
+                    },
+
+                    opponentData = new MatchInitUserDto
+                    {
+                        matchId = init.MatchId,
+                        opponentHandSize = init.Player1DrawnCards.Count,
+
+                        self = new MatchInitSideDto
+                        {
+                            position = init.Player2Position,
+                            champion = p2Champ,
+                            gold = p2Gold,
+                            secondaryCurrencyName = secondaryName,
+                            secondaryCurrency = p2Secondary,
+                            drawnCards = p2Cards
+                        },
+
+                        opponent = new MatchInitSideDto
+                        {
+                            position = init.Player1Position,
+                            champion = p1Champ,
+                            gold = p1Gold,
+                            secondaryCurrencyName = secondaryName,
+                            secondaryCurrency = p1Secondary,
+                            drawnCards = Array.Empty<MatchInitCardDto>()
+                        }
+                    }
+                };
+
+            await CallManager.Instance.CallAsync(payload, ct);
         }
-
     }
 
     public static Task LeaveQueueAsync(UserId userId, CancellationToken ct = default)
-        => RoomManager.Instance.Matchmaker.LeaveQueueAsync(userId, ct);
+    {
+        return RoomManager.Instance.Matchmaker.LeaveQueueAsync(userId, ct);
+    }
 }
--- server/apps/game/Domaine/Match/Agregate/Match.cs
+++ server/apps/game/Domaine/Match/Agregate/Match.cs
@@ -1,7 +1,11 @@
-﻿namespace game.Domaine.Match.Entity;
+﻿using game.Domaine.Match.Agregate;
 
-using ValueObject;
+namespace game.Domaine.Match.Entity;
 
+using System;
+using System.Collections.Generic;
+using game.Domaine.Interface;
+using game.Domaine.Match.ValueObject;
 
 public sealed class Match
 {
@@ -12,20 +16,84 @@ public sealed class Match
 
     public bool IsFinished { get; private set; }
 
+    private readonly List<IEvent> _events = new List<IEvent>();
+
     public Match(Player p1, Player p2)
     {
         Player1 = p1;
         Player2 = p2;
     }
 
+    public void InitMatch()
+    {
+        IsFinished = false;
+
+        Random rng = Random.Shared;
+
+        Player1.Deck.Shuffle(rng);
+        Player2.Deck.Shuffle(rng);
+
+        Player1.Champion.Gold = new ChampionGold(Player1.Champion.BaseGold.Value);
+        Player2.Champion.Gold = new ChampionGold(Player2.Champion.BaseGold.Value);
+
+        List<GameCardDto> p1Drawn = DrawCards(Player1, 6);
+        List<GameCardDto> p2Drawn = DrawCards(Player2, 5);
+
+        MatchInitData initData = new MatchInitData(
+            MatchId.Value,
+            (Guid)Player1.UserId,
+            (Guid)Player2.UserId,
+            1,
+            2,
+            Player1.Champion,
+            Player2.Champion,
+            p1Drawn,
+            p2Drawn
+        );
+
+        _events.Add(new DomainEvent(MatchEvent.MATCH_INIT, initData));
+    }
+
+    public IReadOnlyList<IEvent> PullEvents()
+    {
+        if (_events.Count == 0)
+        {
+            return Array.Empty<IEvent>();
+        }
+
+        IEvent[] batch = _events.ToArray();
+        _events.Clear();
+        return batch;
+    }
+
+    private static List<GameCardDto> DrawCards(Player player, int count)
+    {
+        List<GameCardDto> drawn = new List<GameCardDto>(count);
+
+        int i = 0;
+        while (i < count)
+        {
+            GameCardDto? card = player.Deck.DrawOne();
+            if (card == null)
+            {
+                break;
+            }
+
+            player.Hand.Add(card);
+            drawn.Add(card);
+            i++;
+        }
+
+        return drawn;
+    }
     public bool HasUser(UserId userId)
-        => Player1.UserId.Equals(userId) || Player2.UserId.Equals(userId);
+    {
+        return Player1.UserId.Equals(userId) || Player2.UserId.Equals(userId);
+    }
 
     public void Start()
     {
-        // TODO: état de tour, mana, etc.
         IsFinished = false;
     }
 
-    public void Finish() => IsFinished = true;
 }
