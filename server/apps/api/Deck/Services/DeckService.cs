4966174312b34d113f4dd02a72315ddea32e7838
Ultiorra <jarreaumik@hotmail.com>
2026-03-27 12:03:47 +0100

    [FEAT] Get deck  data by UserID

--- server/apps/api/Deck/Services/DeckService.cs
+++ server/apps/api/Deck/Services/DeckService.cs
@@ -4,9 +4,10 @@ using VortexTCG.Api.Deck.Interface;
 using VortexTCG.Api.Deck.Providers;
 using VortexTCG.Common.DTO;
 using DeckModel = VortexTCG.DataAccess.Models.Deck;
-using CardModel = VortexTCG.DataAccess.Models.Card;
 using DeckCardModel = VortexTCG.DataAccess.Models.DeckCard;
 using CollectionCardModel = VortexTCG.DataAccess.Models.CollectionCard;
+using CardModel = VortexTCG.DataAccess.Models.Card;
+
 namespace VortexTCG.Api.Deck.Services
 {
     public class DeckService : IDeckService
@@ -18,13 +19,25 @@ namespace VortexTCG.Api.Deck.Services
             _deckProvider = deckProvider;
         }
 
-        public ResultDTO<DeckDTO> GetDeckById(string deckId)
-        => new ResultDTO<DeckDTO> {
-            success = true,
-            statusCode = 200,
-            data = _deckProvider.GetMockDeck(deckId)
-        };
-        
+        public async Task<ResultDTO<List<DeckDTO>>> GetDecksByUserIdAsync(Guid userId)
+        {
+            var decks = await _deckProvider.GetDecksByUserIdAsync(userId);
+
+            var result = decks.Select(d => new DeckDTO
+            {
+                Id = d.Id.ToString(),
+                Name = d.Label,
+                Champion = d.Champion == null ? null : MapToDeckChampionDto(d.Champion)
+            }).ToList();
+
+            return new ResultDTO<List<DeckDTO>>
+            {
+                success = true,
+                statusCode = 200,
+                data = result
+            };
+        }
+
         public async Task<ResultDTO<DeckDataDto>> GetDeckDataAsync(Guid deckId)
         {
             DeckModel deck = await _deckProvider.GetDeckWithCardsAndChampionAsync(deckId);
@@ -39,10 +52,12 @@ namespace VortexTCG.Api.Deck.Services
                     data = null
                 };
             }
+
             List<Guid> cardIds = deck.DeckCard
                 .Select(dc => dc.Card.CardId)
                 .Distinct()
                 .ToList();
+
             List<(Guid CardId, string Label)> classRows = await _deckProvider.GetClassRowsByCardIdsAsync(cardIds);
 
             Dictionary<Guid, List<string>> classMap = classRows
@@ -52,6 +67,7 @@ namespace VortexTCG.Api.Deck.Services
                     g => g.Key,
                     g => g.Select(t => t.Label).Distinct().ToList()
                 );
+
             List<DeckCardDto> cards = deck.DeckCard.Select((DeckCardModel dc) =>
             {
                 CollectionCardModel cc = dc.Card;
@@ -65,10 +81,8 @@ namespace VortexTCG.Api.Deck.Services
                 {
                     DeckCardId = dc.Id,
                     Quantity = dc.Quantity,
-
                     CollectionCardId = cc.Id,
                     Rarity = cc.Rarity,
-
                     CardId = c.Id,
                     Name = c.Name,
                     Hp = c.Hp,
@@ -79,7 +93,6 @@ namespace VortexTCG.Api.Deck.Services
                     Extension = c.Extension,
                     CardType = c.CardType,
                     Price = c.Price,
-
                     Classes = classes
                 };
             }).ToList();
