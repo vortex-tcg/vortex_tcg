75895df5a5acb80990261dc392b8fdd44d62b8e1
Ultiorra <mikael.jarreau@epitech.eu>
2026-05-15 17:10:40 +0200

    [FEAT] test game application

--- server/tests/game/Match/Application/QueueServiceTests.cs
+++ server/tests/game/Match/Application/QueueServiceTests.cs
@@ -1,12 +1,19 @@
+using System.Collections;
+using System.Reflection;
+using game.Application.Dto;
 using game.Application.Factory;
 using game.Application.Service;
+using game.Domaine.Match.Agregate;
+using game.Domaine.Match.Entity;
 using game.Domaine.Match.ValueObject;
+using game.Domaine.Matchmaking;
 using game.Infrastructure.DTO;
 using game.Infrastructure.Interface;
 using game.Infrastructure.Manager;
 using game.Tests.Helpers;
 using Microsoft.AspNetCore.SignalR;
 using Moq;
+using MatchAggregate = game.Domaine.Match.Agregate.Match;
 
 namespace game.Tests.Domain.Application;
 
@@ -111,5 +118,98 @@ public class QueueServiceTests : IDisposable
             It.IsAny<string>(),
             It.IsAny<object[]>(),
             It.IsAny<CancellationToken>()), Times.AtLeastOnce());
+    }
+
+    [Fact]
+    public async Task JoinQueueAsync_SkipsNonFoundEvents_InLoop()
+    {
+        object matchmaker = RoomManager.Instance.Matchmaker;
+        IList matchmakerEvents = (IList)matchmaker.GetType()
+            .GetField("_events", BindingFlags.NonPublic | BindingFlags.Instance)!
+            .GetValue(matchmaker)!;
+        matchmakerEvents.Add(new MatchmakerEvent("OTHER_EVENT", new object()));
+
+        await QueueService.JoinQueueAsync(new UserId(Guid.NewGuid()), new DeckId(Guid.NewGuid()));
+
+        _mockProxy.Verify(p => p.SendCoreAsync(
+            It.IsAny<string>(),
+            It.IsAny<object[]>(),
+            It.IsAny<CancellationToken>()), Times.Never());
+    }
+
+    [Fact]
+    public void InitMatch_AlwaysProducesMatchInitEvent()
+    {
+
+        MatchAggregate match = MatchHelpers.MakeMatchInPhase<StandByPhase>();
+        match.PullEvents();
+
+        match.InitMatch();
+
+        var events = match.PullEvents();
+        Assert.Equal(MatchEvent.MATCH_INIT, events[0].Name);
+    }
+
+    [Fact]
+    public async Task JoinQueueAsync_MapsDrawnCardsForBothPlayers()
+    {
+        Mock<IDeckApiClient> mockClient = new Mock<IDeckApiClient>();
+        mockClient
+            .Setup(c => c.GetDeckDataAsync(It.IsAny<DeckId>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(BuildApiDeckWithMultipleCards(15));
+        RoomManager.Configure(new CreateMatchFactory(mockClient.Object));
+
+        List<object[]> capturedArgs = new();
+        _mockProxy
+            .Setup(p => p.SendCoreAsync(It.IsAny<string>(), It.IsAny<object[]>(), It.IsAny<CancellationToken>()))
+            .Callback<string, object[], CancellationToken>((_, args, _) => capturedArgs.Add(args))
+            .Returns(Task.CompletedTask);
+
+        await QueueService.JoinQueueAsync(new UserId(Guid.NewGuid()), new DeckId(Guid.NewGuid()));
+        await QueueService.JoinQueueAsync(new UserId(Guid.NewGuid()), new DeckId(Guid.NewGuid()));
+
+        List<MatchInitUserDto> dtos = capturedArgs
+            .Where(a => a.Length > 0 && a[0] is MatchInitUserDto)
+            .Select(a => (MatchInitUserDto)a[0])
+            .ToList();
+
+        Assert.Equal(2, dtos.Count);
+        Assert.All(dtos, dto => Assert.NotEmpty(dto.self.drawnCards));
+    }
+
+    private static ApiDeckDataDto BuildApiDeckWithMultipleCards(int count)
+    {
+        List<ApiDeckCardDto> cards = new();
+        for (int i = 0; i < count; i++)
+        {
+            cards.Add(new ApiDeckCardDto
+            {
+                DeckCardId = Guid.NewGuid(),
+                Quantity = 1,
+                CollectionCardId = Guid.NewGuid(),
+                CardId = Guid.NewGuid(),
+                Name = $"Card{i}",
+                Hp = 3,
+                Attack = 2,
+                Cost = 1,
+                Description = "Test",
+                Picture = "pic.png",
+                CardType = 0,
+                Classes = new List<string>()
+            });
+        }
+        return new ApiDeckDataDto
+        {
+            Champion = new ApiDeckChampionDto
+            {
+                ChampionID = Guid.NewGuid(),
+                Name = "TestHero",
+                Description = "desc",
+                HP = 30,
+                Picture = "pic.png",
+                FactionId = Guid.NewGuid()
+            },
+            Cards = cards
+        };
     }
 }
